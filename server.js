4e9357169e8209a93dbcbb9b61864d4ec1e52148
Eve Montalvao <eve.ionica@gmail.com>
2017-08-20 12:14:06 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var 
+http = require('http'),
+express = require('express'),
+path = require('path');
+fs = require('fs'),
+app = express(),
+articles = process.cwd() + '/article/';
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+http.createServer(app).listen(2107);
+
+console.log("Server listening on port 2107");
+
