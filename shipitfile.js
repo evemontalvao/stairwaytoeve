4fa6d694720d94e4afab53458637809ae4c56105
Eve Montalvao <eve.ionica@gmail.com>
2017-08-22 21:08:05 -0300

    Fix: shipitfile

--- shipitfile.js
+++ shipitfile.js
@@ -7,12 +7,8 @@ module.exports = function (shipit) {
             repositoryUrl: 'git@github.com:evemontalvao/stairwaytoeve.git',
             ignores: ['.git', 'node_modules', 'dev', 'articles'],
             keepReleases: 5,
-<<<<<<< HEAD
-            servers: 'eve@138.197.28.94'
-=======
             servers: 'eve@138.197.28.94',
             dirToCopy: '/public'
->>>>>>> b2e46d848ce5837ea58c7be3dd547dac8c55cc2b
         },
         production: {
             deployTo: '/home/eve/stairwaytoeve/production',
